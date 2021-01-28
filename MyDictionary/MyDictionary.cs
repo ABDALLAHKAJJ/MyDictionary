bd96738d7f813fccf37b750c6e350cd740fdc30b
Abdallahkajj <busy.heart@live.com>
2021-01-28 20:09:46 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace MyDictionarys
+{
+    public class MyDictionary<T>
+    {
+        private T[] K;
+        private T[] V;
+        private List<int> t = new List<int>();
+
+        public MyDictionary()
+        {
+            K = new T[0];
+            V = new T[0];
+        }
+
+        public void Add(T k, T v)
+        {
+            T[] tempKey = K;
+            T[] tempVlaue = V;
+            K = new T[K.Length + 1];
+            V = new T[V.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                K[i] = tempKey[i];
+                V[i] = tempVlaue[i];
+            }
+
+            K[^1] = k;
+            V[^1] = v;
+        }
+
+        public int Length
+        {
+            get
+            {
+                return K.Length;
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionarys
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+            myDictionary.Add("ali", 1);
+            myDictionary.Add("ali2", 2);
+            myDictionary.Add("ali3", 3);
+            myDictionary.Add("ali4", 4);
+
+            foreach (var item in myDictionary.Items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
